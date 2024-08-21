e2eaa4f91f15e3c4e0415a568960e38afaeb4594
Hyunjung Seo <153152990+Hyun-Jungseo@users.noreply.github.com>
2024-08-22 01:16:07 +0900

    first commit

---
+++ src/component/ProductCard.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const ProductCard = () => {
+  return (
+    <div>
+      <img src='https://lp2.hm.com/hmgoepprod?set=quality%5B79%5D%2Csource%5B%2F08%2F04%2F08041ec973a2cdb4b55a841da226b5eee3ce5d54.jpg%5D%2Corigin%5Bdam%5D%2Ccategory%5B%5D%2Ctype%5BLOOKBOOK%5D%2Cres%5Bm%5D%2Chmver%5B1%5D&call=url[file:/product/main]' />
+      <div>Conscious choice</div>
+      <div>벨티드 트윌 코트</div>
+      <div>₩99900</div>
+      <div>신제품</div>
+    </div>
+  )
+}
+
+export default ProductCard
